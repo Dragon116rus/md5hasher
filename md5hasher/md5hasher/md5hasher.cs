8921992dd3022801c9c0bcf5e83fe2576b8f332e
Dragon116rus <dragon116ru@yandex.ru>
2016-07-21 09:09:12 +0300

    added third step

--- md5hasher/md5hasher/md5hasher.cs
+++ md5hasher/md5hasher/md5hasher.cs
@@ -8,13 +8,14 @@ namespace md5hasher
 {
     class Md5hasher
     {
-       byte[] sourceBytes;
-       public string hash(string sourcePhrase)
+        byte[] sourceBytes;
+        public string hash(string sourcePhrase)
         {
             firstStep(sourcePhrase);
             secondStep(sourcePhrase);
-            // thirdStep(sourcePhrase);
-            // fourhStep(sourcePhrase);
+            // thirdStep();
+            // fourhStep();
+
             return sourcePhrase;
         }
         #region firstStep
@@ -22,8 +23,8 @@ namespace md5hasher
         {
             byte[] intermediate = toByteArray(sourcePhrase);
             sourceBytes = new byte[getSourceBytesLength(sourcePhrase)];
-            copyIntermediateInSource(intermediate,sourceBytes);
-            insertSolitaryByte(sourceBytes,intermediate.Length);
+            copyIntermediateInSource(intermediate, sourceBytes);
+            insertSolitaryByte(sourceBytes, intermediate.Length);
         }
         private byte[] toByteArray(string phrase)
         {
@@ -34,17 +35,17 @@ namespace md5hasher
         private int getSourceBytesLength(string phrase)
         {
             int result = 512 + 448;
-            while(result<=phrase.Length*sizeof(char)+1)
+            while (result <= phrase.Length * sizeof(char) + 1)
             {
                 result += 512;
             }
             return result;
         }
-        private void copyIntermediateInSource(byte[] intermediate,byte[] source)
+        private void copyIntermediateInSource(byte[] intermediate, byte[] source)
         {
             System.Buffer.BlockCopy(intermediate, 0, source, 0, intermediate.Length);
         }
-        private void insertSolitaryByte(byte[] sourceBytes,int position)
+        private void insertSolitaryByte(byte[] sourceBytes, int position)
         {
             sourceBytes[position] = 0x80;
         }
@@ -65,6 +66,53 @@ namespace md5hasher
                 Array.Reverse(lengthInBytes);
             }
             return lengthInBytes;
+        }
+        #endregion
+        #region thirdStep
+        Int64 wordA = 0x67452301,
+              wordB = 0xefcdab89,
+              wordC = 0x98badcfe,
+              wordD = 0x10325476;
+        Int64[] s = {
+            7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22 ,
+            5,  9, 14, 20,  5,  9, 14, 20,  5,  9, 14, 20,  5,  9, 14, 20,
+            4, 11, 16, 23,  4, 11, 16, 23,  4, 11, 16, 23,  4, 11, 16, 23,
+            6, 10, 15, 21,  6, 10, 15, 21,  6, 10, 15, 21,  6, 10, 15, 21
+        };
+        Int64[] K =
+        {
+            0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee,
+            0xf57c0faf, 0x4787c62a, 0xa8304613, 0xfd469501,
+            0x698098d8, 0x8b44f7af, 0xffff5bb1, 0x895cd7be,
+            0x6b901122, 0xfd987193, 0xa679438e, 0x49b40821,
+            0xf61e2562, 0xc040b340, 0x265e5a51, 0xe9b6c7aa,
+            0xd62f105d, 0x02441453, 0xd8a1e681, 0xe7d3fbc8,
+            0x21e1cde6, 0xc33707d6, 0xf4d50d87, 0x455a14ed,
+            0xa9e3e905, 0xfcefa3f8, 0x676f02d9, 0x8d2a4c8a,
+            0xfffa3942, 0x8771f681, 0x6d9d6122, 0xfde5380c,
+            0xa4beea44, 0x4bdecfa9, 0xf6bb4b60, 0xbebfbc70,
+            0x289b7ec6, 0xeaa127fa, 0xd4ef3085, 0x04881d05,
+            0xd9d4d039, 0xe6db99e5, 0x1fa27cf8, 0xc4ac5665,
+            0xf4292244, 0x432aff97, 0xab9423a7, 0xfc93a039,
+            0x655b59c3, 0x8f0ccc92, 0xffeff47d, 0x85845dd1,
+            0x6fa87e4f, 0xfe2ce6e0, 0xa3014314, 0x4e0811a1,
+            0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391
+        };
+        Int32 funF(Int32 x, Int32 y, Int32 z)
+        {
+            return (x | y) & (~x | z);
+        }
+        Int32 funG(Int32 x, Int32 y, Int32 z)
+        {
+            return (x | z) & (~z | y);
+        }
+        Int32 funH(Int32 x, Int32 y, Int32 z)
+        {
+            return x ^ y ^ z;
+        }
+        Int32 funI(Int32 x, Int32 y, Int32 z)
+        {
+            return y ^ ((~z) & x);
         }
         #endregion
     }
