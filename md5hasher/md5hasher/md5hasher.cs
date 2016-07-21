3404814053fe9a1b62cd5ce9a622566bed81de65
Dragon116rus <dragon116ru@yandex.ru>
2016-07-21 15:32:00 +0300

    added fourth step

--- md5hasher/md5hasher/md5hasher.cs
+++ md5hasher/md5hasher/md5hasher.cs
@@ -13,9 +13,8 @@ namespace md5hasher
         {
             firstStep(sourcePhrase);
             secondStep(sourcePhrase);
-            // fourthStep();
-
-            return sourcePhrase;
+            string result = fourthStep();
+            return result;
         }
         #region firstStep
         private void firstStep(string sourcePhrase)
@@ -52,7 +51,7 @@ namespace md5hasher
         #region secondStep
         private void secondStep(string sourcePhrase)
         {
-            uint lengthOfPhraseInBits =(uint) sourcePhrase.Length*sizeof(char)*8;
+            uint lengthOfPhraseInBits = (uint)sourcePhrase.Length * sizeof(char) * 8;
             int startedPositionToInsert = sourceBytes.Length - 8;
             byte[] lengthInBytes = getLengthInBytes(lengthOfPhraseInBits);
             System.Buffer.BlockCopy(lengthInBytes, 0, sourceBytes, startedPositionToInsert, lengthInBytes.Length);
@@ -69,9 +68,9 @@ namespace md5hasher
         #endregion
         #region thirdStep
         uint wordA = 0x67452301,
-              wordB = 0xefcdab89,
-              wordC = 0x98badcfe,
-              wordD = 0x10325476;
+             wordB = 0xefcdab89,
+             wordC = 0x98badcfe,
+             wordD = 0x10325476;
         uint[] s = {
             7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22 ,
             5,  9, 14, 20,  5,  9, 14, 20,  5,  9, 14, 20,  5,  9, 14, 20,
@@ -115,32 +114,137 @@ namespace md5hasher
         }
         #endregion
         #region fourthStep
-        private void fourthStep()
+        private string fourthStep()
         {
-            for (int iteration = 0; iteration < getCountOfIteration(); iteration++ )
+            for (int iteration = 0; iteration < getCountOfIteration(); iteration++)
             {
                 uint wordAA = wordA;
                 uint wordBB = wordB;
                 uint wordCC = wordC;
                 uint wordDD = wordD;
-                
+                uint[] X = blockInAnArray(iteration);
+                stage1(ref wordAA, ref wordBB, ref wordCC, ref wordDD, X);
+                stage2(ref wordAA, ref wordBB, ref wordCC, ref wordDD, X);
+                stage3(ref wordAA, ref wordBB, ref wordCC, ref wordDD, X);
+                stage4(ref wordAA, ref wordBB, ref wordCC, ref wordDD, X);
+                wordA += wordAA;
+                wordB += wordBB;
+                wordC += wordCC;
+                wordD += wordDD;
             }
-            
+            string result = glueAnswer(wordA, wordB, wordC, wordD);
+            return result;
         }
         private int getCountOfIteration()
         {
             return (sourceBytes.Length * 8) / 512;
         }
-        private void stage1(ref uint wordAA, ref uint wordBB,ref uint wordCC, ref uint wordDD)
+        private uint[] blockInAnArray(int iteration)
         {
-            for (int i = 0; i < 16; i++)
+            uint[] array = new uint[16];
+            int startPosition = (iteration * 512) / 8;
+            for (int i = 0; i < array.Length; i++)
             {
-
+                array[i] = BitConverter.ToUInt32(sourceBytes, startPosition);
+                startPosition++;
+            }
+            return array;
+        }
+        private void stage1(ref uint wordAA, ref uint wordBB, ref uint wordCC, ref uint wordDD, uint[] X)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                wordAA = helpFunction(wordAA, wordBB, wordCC, wordDD, i * 4 + 0, 7, i * 4, X, funF);
+                wordBB = helpFunction(wordDD, wordAA, wordBB, wordCC, i * 4 + 1, 12, i * 4 + 1, X, funF);
+                wordCC = helpFunction(wordCC, wordDD, wordAA, wordBB, i * 4 + 2, 17, i * 4 + 2, X, funF);
+                wordDD = helpFunction(wordBB, wordCC, wordDD, wordAA, i * 4 + 3, 22, i * 4 + 3, X, funF);
             }
         }
-        private uint functionForStage1(uint a, uint b , uint c , uint d,)
+        private void stage2(ref uint wordAA, ref uint wordBB, ref uint wordCC, ref uint wordDD, uint[] X)
         {
-            return b + ((a + funF(b, c, d) + X[k] + T[i]) <<< s);
+            for (int i = 0; i < 4; i++)
+            {
+                wordAA = helpFunction(wordAA, wordBB, wordCC, wordDD, (i * 4 + 1) % 16, 5, i * 4 + 17, X, funG);
+                wordBB = helpFunction(wordDD, wordAA, wordBB, wordCC, (i * 4 + 6) % 16, 9, i * 4 + 18, X, funG);
+                wordCC = helpFunction(wordCC, wordDD, wordAA, wordBB, (i * 4 + 11) % 16, 14, i * 4 + 19, X, funG);
+                wordDD = helpFunction(wordBB, wordCC, wordDD, wordAA, (i * 4 + 0) % 16, 20, i * 4 + 20, X, funG);
+            }
+        }
+        private void stage3(ref uint wordAA, ref uint wordBB, ref uint wordCC, ref uint wordDD, uint[] X)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                wordAA = helpFunction(wordAA, wordBB, wordCC, wordDD, (i * 12 + 5) % 16, 4, i * 4 + 33, X, funH);
+                wordBB = helpFunction(wordDD, wordAA, wordBB, wordCC, (i * 12 + 8) % 16, 11, i * 4 + 34, X, funH);
+                wordCC = helpFunction(wordCC, wordDD, wordAA, wordBB, (i * 12 + 11) % 16, 16, i * 4 + 35, X, funH);
+                wordDD = helpFunction(wordBB, wordCC, wordDD, wordAA, (i * 12 + 14) % 16, 23, i * 4 + 36, X, funH);
+            }
+        }
+        private void stage4(ref uint wordAA, ref uint wordBB, ref uint wordCC, ref uint wordDD, uint[] X)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                wordAA = helpFunction(wordAA, wordBB, wordCC, wordDD, (i * 12 + 0) % 16, 6, i * 4 + 49, X, funI);
+                wordBB = helpFunction(wordDD, wordAA, wordBB, wordCC, (i * 12 + 7) % 16, 10, i * 4 + 50, X, funI);
+                wordCC = helpFunction(wordCC, wordDD, wordAA, wordBB, (i * 12 + 14) % 16, 15, i * 4 + 51, X, funI);
+                wordDD = helpFunction(wordBB, wordCC, wordDD, wordAA, (i * 12 + 5) % 16, 21, i * 4 + 52, X, funI);
+            }
+        }
+        private uint helpFunction(uint a, uint b, uint c, uint d, int k, int i, int s, uint[] X, Func<uint, uint, uint, uint> function)
+        {
+            return b + cyclicShift((a + function(b, c, d) + X[k] + K[i]), s);
+        }
+        private uint cyclicShift(uint value, int countOfBits)
+        {
+            uint result = value << countOfBits;
+            result |= value >> (sizeof(uint) * 8 - countOfBits);
+            return result;
+        }
+        private string glueAnswer(uint wordA, uint wordB, uint wordC, uint wordD)
+        {
+            byte[] answerInBytes = new byte[16];
+            byte[] aInBytes = BitConverter.GetBytes(wordA);
+            byte[] bInBytes = BitConverter.GetBytes(wordB);
+            byte[] cInBytes = BitConverter.GetBytes(wordC);
+            byte[] dInBytes = BitConverter.GetBytes(wordD);
+
+            Buffer.BlockCopy(aInBytes, 0, answerInBytes, 0, aInBytes.Length);
+            Buffer.BlockCopy(bInBytes, 0, answerInBytes, 4, bInBytes.Length);
+            Buffer.BlockCopy(cInBytes, 0, answerInBytes, 8, cInBytes.Length);
+            Buffer.BlockCopy(dInBytes, 0, answerInBytes, 12, dInBytes.Length);
+
+            StringBuilder answer = new StringBuilder();
+            foreach (byte b in answerInBytes)
+            {
+                answer.Append(getStringImage(b));
+            }
+            return answer.ToString();
+        }
+        private string getStringImage(byte b)
+        {
+            int first = b / 16;
+            int second = b % 16;
+            string answer = getHexDigit(first) + getHexDigit(second);
+            return answer;
+        }
+        private string getHexDigit(int a)
+        {
+            if (a < 10)
+                return a.ToString();
+
+            if (a == 10)
+                return "A";
+            if (a == 11)
+                return "B";
+            if (a == 12)
+                return "C";
+            if (a == 13)
+                return "D";
+            if (a == 14)
+                return "E";
+            if (a == 15)
+                return "F";
+            return "?";
         }
         #endregion
     }
